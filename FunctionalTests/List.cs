0c20b2d6821adb0494b5bfb4208bf201dae8f97e
Dmitry Arapov <Dmitry.Arapov@raiffeisen.ru>
2020-10-13 00:45:32 +0300

    first commit

---
+++ FunctionalTests/List.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Reflection;
+
+namespace FunctionalTests
+{
+  public class List<T>
+  {  
+    public virtual T Head { get; }
+    public virtual List<T> Tail { get; private set; }
+    
+    public static List<T> Nil { get; } = new NilList<T>();
+
+    public List(T value)
+    {
+      Head = value;
+      Tail = null;
+    }
+
+    public List<T> Prepend(T value)
+    {
+      return new List<T>(value)
+      {
+        Tail = this
+      };
+    }
+
+    public List<K> Map<K>(Func<T, K> func)
+    {
+      return this switch
+      {
+        NilList<T> _ => List<K>.Nil,
+        List<T> _ => Tail.Map(func).Prepend(func(Head))
+      };
+    }
+
+    public List<K> MapWithReduce<K>(Func<T, K> func)
+    {
+      return ReduceFromRight((accum, cur) => accum.Prepend(func(cur)), List<K>.Nil);
+    }
+
+    public List<T> Filter(Func<T, bool> condition)
+    {
+      return ReduceFromRight((accum, cur) =>
+        condition(cur) switch
+        {
+          true => accum.Prepend(cur),
+          false => accum
+        }
+      , Nil);
+    }
+
+    public (List<T>, List<T>) Partition(Func<T, bool> condition)
+    {
+      return ReduceFromRight((tuple, cur) =>
+        condition(cur) switch
+        {
+          true => (tuple.Item1.Prepend(cur), tuple.Item2),
+          false => (tuple.Item1, tuple.Item2.Prepend(cur))
+        }, (Nil, Nil));
+    }
+
+    public K ReduceFromRight<K>(Func<K, T, K> reducer, K initial)
+    {
+      return this switch
+      {
+        NilList<T> _ => initial,
+        List<T> _ => reducer(Tail.ReduceFromRight(reducer, initial), Head)
+      };
+    }
+
+    public K ReduceFromLeft<K>(Func<K, T, K> reducer, K initial)
+    {
+      return this switch
+      {
+        NilList<T> _ => initial,
+        List<T> _ => Tail.ReduceFromLeft(reducer, reducer(initial, Head))
+      };
+    }
+
+    public List<T> ReverseWithReduce()
+    {
+      return ReduceFromLeft((accum, cur) => accum.Prepend(cur), Nil);
+    }
+
+    public List<T> Reverse()
+    {
+      return ReverseInternal(Nil);
+    }
+
+    private List<T> ReverseInternal(List<T> accumulated)
+    {
+      return this switch
+      {
+        NilList<T> _ => accumulated,
+        List<T> _ => Tail.ReverseInternal(accumulated.Prepend(Head))
+      };
+    }
+
+    public void ForEach(Action<T> action)
+    {
+      switch (this)
+      {
+        case NilList<T> _:
+          return;
+        default:
+        {
+          action(Head);
+          Tail.ForEach(action);
+          break;
+        }
+      }
+    }
+  }
+
+  public class NilList<T> : List<T>
+  {
+    public override T Head
+    {
+      get
+      {
+        throw new ArgumentException();
+      }
+    }
+
+    public override List<T> Tail {
+      get
+      {
+        throw new ArgumentException();
+      }  
+    }
+
+    public NilList() : base(default)
+    {
+        
+    }
+  }
+}
---
+++ FunctionalTests/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FunctionalTests
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var list = List<int>.Nil.Prepend(3);
+      var list2 = list.Prepend(5);
+      var list3 = list2.Prepend(7);
+      var mappedList = list3.MapWithReduce(_ => _ * 7);
+
+      mappedList.ForEach(Console.WriteLine);
+      
+      var reduced = mappedList.ReduceFromLeft((sum, cur) => sum + cur, 0);
+      Console.WriteLine(reduced);
+      
+      Console.WriteLine("-------- unreversed list3 ------");
+      list3.ForEach(Console.WriteLine);
+      Console.WriteLine("------ reversed list3 --------");
+      list3.ReverseWithReduce().ForEach(Console.WriteLine);
+
+
+      var list4 = list3.Prepend(13);
+      var (lessThan7, greaterThan7) = list4.Partition(i => i < 7);
+      Console.WriteLine("less than 7");
+      lessThan7.ForEach(Console.WriteLine);
+      Console.WriteLine("greater than 7");
+      greaterThan7.ForEach(Console.WriteLine);
+    }
+  }
+}
